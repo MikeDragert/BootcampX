6b96f77f96015edc8cfa0db093b03219dc2a3d63
Mike Dragert <mikedragert@gmail.com>
2024-02-05 12:29:26 -0500

    Set up students and cohorts table.  Create seed files for seeding data on these two tables

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,22 @@
+
+DROP TABLE  IF EXISTS students;
+DROP TABLE IF EXISTS cohorts;
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name varchar(255),
+  start_date DATE,
+  end_date DATE
+);
+
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name varchar(255),
+  email varchar(255),
+  phone varchar(32),
+  github varchar(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
